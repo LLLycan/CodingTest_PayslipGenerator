f1d4117eb7ac0ed7d91a8dfc04e214b15c2af3ac
Yuxuan Li <yli89@outlook.com>
2018-10-05 02:26:58 +1000

    1. Initialize app UI 2. Implement exception handler in program main method 3. Ready to connect to business logic

---
+++ CodingExercise.PayslipGenerator/Program.cs
@@ -0,0 +1,39 @@
+﻿using CodingExercise.FileHandler;
+using CodingExercise.PayslipCalculator.Factory;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodingExercise.PayslipGenerator
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            try
+            {
+                Console.WriteLine("Ready to processing file...");
+                Console.ReadLine();
+            }
+            catch (FileNotFoundException)
+            {
+                // Show message of File not exist
+                Console.WriteLine("File not exists! Please provide a valid file path.");
+            }
+            catch (InvalidDataException)
+            {
+                // Show message of dataset format invalid
+                Console.WriteLine("Invalid file data format! Please provide a valid data file.");
+            }
+            catch (Exception)
+            {
+                // Show message of unknow error
+                Console.WriteLine("Unknown error! Please try later.");
+            }
+            Console.ReadKey();
+        }
+    }
+}
